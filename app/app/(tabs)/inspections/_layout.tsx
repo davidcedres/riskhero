7edbcbf991931fc4622aaf899145ea623cc71875
David Cedres <david.cedresx@gmail.com>
2024-01-29 15:56:19 -0400

    move layouts to stacks

--- app/app/(tabs)/inspections/_layout.tsx
+++ app/app/(tabs)/inspections/_layout.tsx
@@ -1,19 +1,3 @@
-import { Stack } from "expo-router";
+import { Stack } from 'expo-router'
 
-const Layout = () => {
-    return (
-        <Stack>
-            <Stack.Screen name="index" options={{ headerShown: false }} />
-            <Stack.Screen name="[id]" options={{ title: "" }} />
-            <Stack.Screen name="categories/[id]" options={{ title: "" }} />
-            <Stack.Screen
-                name="categories/observations/new"
-                options={{
-                    title: "",
-                }}
-            />
-        </Stack>
-    );
-};
-
-export default Layout;
+export default Stack
--- app/app/_layout.tsx
+++ app/app/_layout.tsx
@@ -1,27 +1,3 @@
-import { Slot } from 'expo-router'
-import { useStore } from '../state/store'
-// import NetInfo from '@react-native-community/netinfo'
-import Notifications from '../components/Notifications'
-// import sync from './sync/sync'
+import { Stack } from 'expo-router'
 
-const Layout = () => {
-    const auth = useStore((store) => store.auth)
-
-    // useEffect(() => {
-    //     const unsubscribe = NetInfo.addEventListener((state) => {
-    //         if (state.isConnected === false || auth.logged === false) return
-    //         void sync()
-    //     })
-
-    //     return () => unsubscribe()
-    // }, [])
-
-    return (
-        <>
-            <Slot />
-            <Notifications />
-        </>
-    )
-}
-
-export default Layout
+export default Stack
